c59541dd80172a9c76bf8dc0191cf01172c71639
komali2 <rogersjcaleb@gmail.com>
2017-01-06 15:55:01 -0800

    its broken

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+
+const webpack = require('webpack');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 
@@ -7,23 +10,22 @@ const PATHS = {
   build: path.join(__dirname, 'build')
 };
 
-const common = merge(
-  {
-    entry: {
-      app: PATHS.app
-    },
-    output: {
-      path: PATHS.build,
-      filename: '[name].js'
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Webpack demo'
-      })
-    ]
-  }
-);
-
 module.exports = function(env) {
+
   return merge(common);
+
+
+  return merge(
+    common,
+    {
+      // Disable performance hints during development
+      performance: {
+        hints: false
+      },
+      plugins: [
+        new webpack.NamedModulesPlugin()
+      ]
+    }
+  );
+
 };
